dd301c56fb3a6d1a0afd957ba916df9e062130a2
博丽灵梦 <@.com>
2025-01-16 17:02:10 +0800

    feat: 优化科室数据处理逻辑，支持动态更新Excel文件
    - 重构了科室数据提取逻辑，直接在循环中创建数据字典，简化了数据结构。
    - 实现了对现有Excel文件的动态更新，支持追加新数据到指定工作表，提升了数据管理的灵活性。
    - 移除了冗余的微创手术数据字段，优化了数据存储格式，增强了代码的可读性和可维护性。

--- 医生工作量.py
+++ 医生工作量.py
@@ -34,38 +34,40 @@ def process_excel_files(folder_path):
             }
             
             # 收集每个科室的数据
-            department_data = {}
             for index, row in df.iterrows():
                 if index >= 2:  # 从第6行开始读取
                     department_name = row[0]  # 科室名称
                     if pd.notna(department_name):  # 确保科室名称不是NaN
-                        if department_name not in department_data:
-                            department_data[department_name] = {
-                                '出院人次': row[indices['出院人次']+2],
-                                '门诊人次': row[indices['门诊人次']+2],
-                                '3级手术': row[indices['3级手术']+2],
-                                '4级手术': row[indices['4级手术']+2],
-                                '3级微创手术': row[indices['3级微创手术']+2],
-                                '4级微创手术': row[indices['4级微创手术']+2],
-                            }
-
-            # 将计算结果添加到部门数据
-            for department, data in department_data.items():
-                data['微创手术'] = data['3级微创手术'] + data['4级微创手术']
-                
-                # 去除data['3级微创手术'] 和 data['4级微创手术']
-                data.pop('3级微创手术')
-                data.pop('4级微创手术')
-            
-
-                # 将数据转化为DataFrame
-                department_df = pd.DataFrame([data])
-                department_df.insert(0, '科室', department)  # 插入科室列
-
-                # 保存到新的Excel文件中
-                output_file_path = os.path.join(output_folder, f'{department}.xlsx')
-                with pd.ExcelWriter(output_file_path, engine='openpyxl', mode='a' if os.path.exists(output_file_path) else 'w') as writer:
-                    department_df.to_excel(writer, sheet_name=date_info, index=False)
+                        data = {
+                            '日期': date_info,
+                            '出院人次': row[indices['出院人次']+2],
+                            '门诊人次': row[indices['门诊人次']+2],
+                            '3级手术': row[indices['3级手术']+2],
+                            '4级手术': row[indices['4级手术']+2],
+                            '3级微创手术': row[indices['3级微创手术']+2],
+                            '4级微创手术': row[indices['4级微创手术']+2],
+                        }
+                        data['微创手术'] = data['3级微创手术'] + data['4级微创手术']
+                        # 去除data['3级微创手术'] 和 data['4级微创手术']
+                        del data['3级微创手术']
+                        del data['4级微创手术']
+                        
+                        # 将数据转化为DataFrame
+                        department_df = pd.DataFrame([data])
+                        
+                        # 保存到新的Excel文件中
+                        output_file_path = os.path.join(output_folder, f'{department_name}.xlsx')
+                        if os.path.exists(output_file_path):
+                            # 读取现有的数据
+                            existing_df = pd.read_excel(output_file_path, sheet_name='工作量明细')
+                            # 追加新的数据
+                            final_df = pd.concat([existing_df, department_df], ignore_index=True)
+                        else:
+                            final_df = department_df
+                        
+                        # 保存到新的Excel文件中
+                        with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
+                            final_df.to_excel(writer, sheet_name='工作量明细', index=False)
 
 # 使用方法
 folder_path = r'C:\Users\biyun\Desktop\新建文件夹'  # 替换为你的文件夹地址
