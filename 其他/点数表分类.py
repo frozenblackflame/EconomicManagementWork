76823e23f1f94199e75aff2e1e9cdcd3c7a6200f
博丽灵梦 <@.com>
2025-03-31 14:51:41 +0800

    feat: 更新Excel处理脚本，修正注释及分类逻辑，添加列重命名功能

--- 其他/点数表分类.py
+++ 其他/点数表分类.py
@@ -4,7 +4,7 @@ import os
 # 读取Excel文件
 df = pd.read_excel(r"C:\Users\biyun\Desktop\work\点值数.xlsx")
 
-# 1. 按照B列(xxx科)分类创建工作表
+# 1. 按照B列(科室)分类创建工作表
 # 获取B列中所有唯一的科室名称
 departments = df['B'].unique()
 
@@ -16,10 +16,7 @@ with pd.ExcelWriter(output_path) as writer:
         # 获取该科室的所有数据
         dept_data = df[df['B'] == dept].copy()
         
-        # 2. 根据D列(医技/医师/空白)进行分类
-        # 首先填充空白值，便于分类
-        dept_data['D'] = dept_data['D'].fillna('空白')
-        
+        # 2. 根据D列进行分类
         # 按D列排序，这样相同类型的会排在一起
         dept_data = dept_data.sort_values('D')
         
@@ -29,8 +26,12 @@ with pd.ExcelWriter(output_path) as writer:
         
         for index, row in dept_data.iterrows():
             current_type = row['D']
-            if previous_type is not None and current_type != previous_type:
-                # 添加空行
+            # 比较时考虑 NaN 的情况
+            if previous_type is not None and pd.notna(current_type) and pd.notna(previous_type):
+                if current_type != previous_type:
+                    empty_row = pd.Series([None] * len(row), index=row.index)
+                    rows_to_add.append((index, empty_row))
+            elif previous_type is not None and (pd.isna(current_type) != pd.isna(previous_type)):
                 empty_row = pd.Series([None] * len(row), index=row.index)
                 rows_to_add.append((index, empty_row))
             previous_type = current_type
@@ -40,6 +41,17 @@ with pd.ExcelWriter(output_path) as writer:
             dept_data = pd.concat([dept_data.iloc[:idx], 
                                  pd.DataFrame([empty_row]), 
                                  dept_data.iloc[idx:]]).reset_index(drop=True)
+        
+        # 重命名列
+        column_mapping = {
+            'A': '核算单元名称',
+            'B': '科室名称',
+            'C': '科室名称',
+            'D': '名称',
+            'E': '项目名称',
+            'F': '点数'
+        }
+        dept_data = dept_data.rename(columns=column_mapping)
         
         # 将处理后的数据写入到对应的工作表中
         dept_data.to_excel(writer, sheet_name=dept, index=False)
